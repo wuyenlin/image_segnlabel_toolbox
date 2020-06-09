204f5858f731bbb1706b5084e4eac233b1cb4687
wuyenlin <finne0831@gmail.com>
2020-06-09 11:04:26 +0800

    resolved imwrite error by converting string to raw string

--- utils/save_seg.py
+++ utils/save_seg.py
@@ -16,10 +16,11 @@ def save_seg(target_img, model_name):
         x1 , y1 = int(pts[i][0][0]), int(pts[i][0][1])
         x2 , y2 = int(pts[i][1][0]), int(pts[i][1][1])
         crop_img = img[y1:y2, x1:x2]
+
         img_name = dt + '_' + f"{i:03}" + '.jpg'
-        out_path = r'{}'.format(os.path.join('PATCHES', date, img_name))
-        is_written = cv2.imwrite(out_path, crop_img)
-        print(is_written)
+        out_path = os.path.join('PATCHES', date, img_name)
+        is_written = cv2.imwrite(out_path.encode('unicode-escape').decode(), crop_img)
+        print("Saved as {}.".format(out_path))
         if is_written:
             print("Saved {}/{}".format(i, len(pts)-1))
         else:
