9e3eb18e1e2d45331791c4b7e01587dd223d7d19
sigtot <sigurdtotland@gmail.com>
2020-06-12 19:58:43 +0200

    Fix previously labeled logic

--- utils/write_lab.py
+++ utils/write_lab.py
@@ -8,29 +8,29 @@ def write_label(seg_img_folder):
     txt_file_r = open(txt_path, 'r')
     labeled = [line.split()[0] for line in txt_file_r.readlines()]
     txt_file_r.close()
-    files_to_label = [file for file in os.listdir(seg_img_folder) if file not in labeled]
+    img_files = [os.path.join(seg_img_folder, filename) for filename in os.listdir(seg_img_folder)]
+    files_to_label = [file for file in img_files if file not in labeled]
     print(f"{len(labeled)} labeled. {len(files_to_label)} to go.")
     print("Keys: 1: car, 0: no car, ESC: quit, ANY OTHER KEY: skip")
 
     txt_file_a = open(txt_path, 'a')
     for file in files_to_label:
-        img_path = os.path.join(seg_img_folder, file)
-        img = cv2.imread(img_path)
+        img = cv2.imread(file)
         cv2.imshow('sample', img)
         cmd = cv2.waitKey()
         if cmd == ord('1'):
-            txt_file_a.write("{} 1\n".format(img_path))
+            txt_file_a.write("{} 1\n".format(file))
             print('1')
             continue
         elif cmd == ord('0'):
-            txt_file_a.write("{} 0\n".format(img_path))
+            txt_file_a.write("{} 0\n".format(file))
             print('0')
             continue
         elif cmd == 27:  # ESC
             print('Stopped.')
             break
         else:  # any other key
-            txt_file_a.write("{} x\n".format(img_path))
+            txt_file_a.write("{} x\n".format(file))
             print('Skipped.')
             continue
 
